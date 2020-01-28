1950326f076b45678fbd7681e69e3f1fe7fb23d9
Dan <daniyar.burakaev@mail.ru>
2020-01-28 19:38:12 +0300

    Accepted

---
+++ pr3.cpp
@@ -0,0 +1,46 @@
+#include "sync.h"
+    
+
+int main(int argc, char* argv[]) {
+    void* shmaddr = NULL;
+    int numOfStr, period, process = 3;
+    
+    numOfStr = std::stoi(argv[1]);
+    period = std::stoi(argv[2]);
+    
+    printf("Process: %d\nNumber of strings: %d\nPeriod of input: %d\n\n", process, numOfStr, period);
+
+    if (shm_id == -1) { shm_id = shmget(SHARED_KEY, sizeof(shared_data), (IPC_CREAT | 0666)); }
+    shmaddr = shmat(shm_id, NULL, 0);
+    shared* shared_var = (shared*) shmaddr;
+    ++(shared_var->processCount);
+    
+    while (true) {
+        sleep(period);
+
+        lock(shared_var, process);
+
+        if (numOfStr > 0) {
+            openFile();
+            
+            fputs("Program #3\n", file);
+            
+            closeFile();
+            --numOfStr;
+        } else {
+            --(shared_var->processCount);
+            unlock(shared_var);
+            break;
+        }
+
+        unlock(shared_var);
+    }
+
+    if (shared_var->processCount == 0) {
+        closeFile();
+        shmdt(shmaddr);
+        shmctl(shm_id, IPC_RMID, NULL);
+    }
+
+    return EXIT_SUCCESS;
+}
--- sync.h
+++ sync.h
@@ -1,64 +1,89 @@
 #pragma once
 
-#define NUM_PROCESS 3
-#define FIRST 0
-#define SECOND 1
-#define THIRD 2
-#define MAX(a,b) (((a)>(b))?(a):(b))
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <string>
+#include <string.h>
+
+#define SHARED_KEY 1001
+
+FILE* file = NULL;
+int shm_id = -1;
 
 typedef struct queue_3 {
 private:
     int _q[3] = {0, 0, 0};
 
 public:
-    void append(int process) {
+    bool is_full() {
         for (int i = 0; i < 3; i++)
-            if (_q[i] != 0)
+            if (_q[0] == 0) return false;
+
+        return true;
+    }
+
+    void append(int process) {
+        int i;
+
+        // printf("Before appending: %d %d %d\n", _q[0], _q[1], _q[2]);
+
+        for (i = 0; i < 3; i++)
+            if (_q[i] == 0) {
                 _q[i] = process;
+                break;
+            }
+
+
+        // printf("After appending: %d %d %d\n", _q[0], _q[1], _q[2]);
+
+        // printf("I: %d\n", i);
     }
 
     int pop() {
+        // printf("Before popping: %d %d %d\n", _q[0], _q[1], _q[2]);
         int left = _q[0];
         _q[0] = _q[1];
         _q[1] = _q[2];
         _q[2] = 0;
-        
+
+        // printf("After popping: %d %d %d\n", _q[0], _q[1], _q[2]);
+
         return left;
     }
-    
-    int peep() { return _q[0]; }
+
+    int peep(int _i = 0) { return _q[_i]; }
 } process_queue;
-        
+
 
 typedef struct shared_data {
-    process_queue queue;
-    
-    int choosing[NUM_PROCESS];
-    int number[NUM_PROCESS];
-    int stop;
+    process_queue accessQueue;
+    int processCount;
+    int terminatedProcesses;
 } shared;
 
-void lock(shared*, int);
-void unlock(shared*, int);
 
-void lock(shared* shared_var, int process) {
-    shared_var->queue.append(process);
-    
-    while(shared_var->queue.peep() != process);/*
-    shared_var->choosing[process] = 1;
-    shared_var->number[process] = 1 + MAX(shared_var->number[FIRST], MAX(shared_var->number[SECOND], shared_var->number[THIRD]));
-    shared_var->choosing[process] = 0;
-    for (int i = 0; i < NUM_PROCESS; ++i) {
-        if (i != process) {
-            while (shared_var->choosing[i]);
-            while (shared_var->number[i] != 0 && 
-                (shared_var->number[process] > shared_var->number[i] ||
-                    (shared_var->number[process] == shared_var->number[i] && process > i))); // + || (number[process] == number[i] && process > i))
-        }
-    }*/
-    /* Критическая секция */
+
+void openFile() {
+    if (shm_id == -1)
+        file = fopen("text.txt", "w");
+    else
+        file = fopen("text.txt", "a");
 }
 
-void unlock(shared* shared_var, int process) {
-    shared_var->queue.pop();
+void closeFile() {
+    fclose(file);
+}
+
+void lock(shared* shared_var, int process) {
+    shared_var->accessQueue.append(process);
+    // printf("Queue: %d %d %d\n", shared_var->accessQueue.peep(0), shared_var->accessQueue.peep(1), shared_var->accessQueue.peep(2));
+    while(shared_var->accessQueue.peep() != process);
+}
+
+void unlock(shared* shared_var) {
+    shared_var->accessQueue.pop();
 }
